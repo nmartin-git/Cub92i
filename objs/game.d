c80ce73316faebc46d13dba3477cbb5e0698a24b
Noe Martin <nmartin@f2r9s16.clusters.42paris.fr>
2025-06-02 18:43:58 +0200

    window opened

--- objs/game.d
+++
@@ -1,8 +0,0 @@
-objs/game.o: game/game.c headers/game.h libft/libft.h \
-  headers/../minilibx-linux/mlx.h
-
-headers/game.h:
-
-libft/libft.h:
-
-headers/../minilibx-linux/mlx.h:
--- objs/game_utils.d
+++
@@ -1,8 +0,0 @@
-objs/game_utils.o: game/game_utils.c headers/game.h libft/libft.h \
-  headers/../minilibx-linux/mlx.h
-
-headers/game.h:
-
-libft/libft.h:
-
-headers/../minilibx-linux/mlx.h:
