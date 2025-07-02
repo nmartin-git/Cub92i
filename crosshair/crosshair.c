256e743781cb55e604fed4b98e3be03ae5500891
Iloy Groussolles <iloy.g32@gmail.com>
2025-07-02 18:33:53 +0200

    add crosshair

---
+++ crosshair/crosshair.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   crosshair.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igrousso <igrousso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/02 17:15:18 by igrousso          #+#    #+#             */
+/*   Updated: 2025/07/02 18:33:19 by igrousso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "crosshair.h"
+
+void	vertical_crosshair(t_pos pixel, t_image *crosshair, int color)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	k = -1;
+	pixel.y = TAB_Y / 2 - (LENGHT_C + GAP_C / 2);
+	while (++k < 2)
+	{
+		j = -1;
+		while (++j < LENGHT_C)
+		{
+			i = -1;
+			pixel.x = TAB_X / 2 - WIDTH_C / 2;
+			while (++i < WIDTH_C)
+			{
+				pixel_put(crosshair, pixel, color);
+				pixel.x++;
+			}
+			pixel.y++;
+		}
+		pixel.y += GAP_C;
+	}
+}
+
+void	horizontal_crosshair(t_pos pixel, t_image *crosshair, int color)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	k = -1;
+	pixel.x = TAB_X / 2 - (LENGHT_C + GAP_C / 2);
+	while (++k < 2)
+	{
+		j = -1;
+		while (++j < LENGHT_C)
+		{
+			i = -1;
+			pixel.y = TAB_Y / 2 - WIDTH_C / 2;
+			while (++i < WIDTH_C)
+			{
+				pixel_put(crosshair, pixel, color);
+				pixel.y++;
+			}
+			pixel.x++;
+		}
+		pixel.x += GAP_C;
+	}
+}
+
+int	init_crosshair(t_data *data)
+{
+	t_image	*crosshair;
+	t_pos	pixel;
+	int		color;
+
+	crosshair = new_image(data->display, TAB_X, TAB_Y);
+	if (!crosshair)
+		return (1);
+    color = CYAN;
+	vertical_crosshair(pixel, crosshair, color);
+	horizontal_crosshair(pixel, crosshair, color);
+    // data->crosshair = crosshair;
+	return (0);
+}
