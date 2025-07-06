9845f5854811326a2b01e8f5c9f0b50aa018b6d4
Iloy Groussolles <iloy.g32@gmail.com>
2025-07-06 19:05:09 +0200

    chicha mystere dans le parsing

--- parsing/map_utils2.c
+++ parsing/map_utils2.c
@@ -6,7 +6,7 @@
 /*   By: igrousso <igrousso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 17:47:47 by igrousso          #+#    #+#             */
-/*   Updated: 2025/07/05 17:10:52 by igrousso         ###   ########.fr       */
+/*   Updated: 2025/07/06 19:04:48 by igrousso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,7 @@ int	ctoi2(char c)
 		return (10);
 	else if (c == 'F')
 		return (11);
+	else if (c == 'C')
+		return (12);
 	return (ft_putstr_fd("Error\nInvalid character\n", 2), -2);
 }
