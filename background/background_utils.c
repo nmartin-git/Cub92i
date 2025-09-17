1826019be5b83ba99d64fcb259346a488498d53d
Iloy Groussolles <iloy.g32@gmail.com>
2025-09-17 10:39:51 +0200

    oublie d'un fichier + fix d'un leak

---
+++ background/background_utils.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   background_utils.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igrousso <igrousso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/17 10:20:25 by igrousso          #+#    #+#             */
+/*   Updated: 2025/09/17 10:36:34 by igrousso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/background.h"
+
+int	decode_r(int rgb)
+{
+	return ((rgb >> 16) & 255);
+}
+
+int	decode_g(int rgb)
+{
+	return ((rgb >> 8) & 255);
+}
+
+int	decode_b(int rgb)
+{
+	return (rgb & 255);
+}
+
+void	init_image(t_image *img)
+{
+	img->image = NULL;
+	img->adress = NULL;
+	img->bpp = -1;
+	img->l_len = -1;
+	img->tab_x = -1;
+	img->tab_y = -1;
+}
+
+int get_adress(t_data *data, t_image *new, t_image *new2)
+{
+	int		null;
+
+	new->adress = mlx_get_data_addr(new->image, &new->bpp, &new->l_len, &null);
+	if (!new->adress)
+	{
+		mlx_destroy_image(data->display, new->image);
+		return (mlx_destroy_image(data->display, new->image), \
+                write(2, "Error\nFail to init\n", 19));
+	}
+    if (data->map->f_rgb == -10)
+    {
+        new2->adress = mlx_get_data_addr(new2->image, &new2->bpp, \
+                                        &new2->l_len, &null);
+	    if (!new2->adress)
+	    {
+	    	mlx_destroy_image(data->display, new2->image);
+	    	return (mlx_destroy_image(data->display, new->image), \
+                    mlx_destroy_image(data->display, new->image), \
+                    write(2, "Error\nFail to init\n", 19));
+	    }
+    }
+    return (0);
+}
